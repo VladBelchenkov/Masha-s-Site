4b7d0f4c5f4831ca4c87752758c60919098088eb
VladBelchenkov <119514834+VladBelchenkov@users.noreply.github.com>
2023-07-01 20:51:25 +0300

    Add files via upload

---
+++ js/zoomable.js
@@ -0,0 +1,25 @@
+function getPics() {} 
+const imgs = document.querySelectorAll('.responsive img');
+const fullPage = document.querySelector('#fullpage');
+
+imgs.forEach(img => {
+  img.addEventListener('click', function() {
+    fullPage.style.backgroundImage = 'url(' + img.src + ')';
+    fullPage.style.display = 'block';
+  });
+}); 
+
+var btn = $('#button');
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+}); 
+ 
+$(".navbar-collapse a").click(function () { 
+  $(".navbar-collapse").collapse("hide");
+}); 
+
